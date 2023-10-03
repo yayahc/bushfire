5cbbbf27ef6bd8a0c6e6f1e13ce0c6113bf55fc5
yayahc <yayahc@localhost.localdomain>
2023-10-03 19:09:30 +0000

    new drawer added

--- simulation/models.py
+++ simulation/models.py
@@ -9,6 +9,15 @@ class X1veg(models.Model):
 
     def __str__(self):
         return self.x1veg
+    
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "X1veg"
+        verbose_name = "X1veg"
+        verbose_name_plural = "X1vegs"
         
 class X2veg(models.Model):
     x2veg = models.FloatField(default=0.0)
@@ -18,6 +27,15 @@ class X2veg(models.Model):
     def __str__(self):
         return self.x2veg
     
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "X2veg"
+        verbose_name = "X2veg"
+        verbose_name_plural = "X2vegs"
+    
 class Y1veg(models.Model):
     y1veg = models.FloatField(default=0.0)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -25,6 +43,15 @@ class Y1veg(models.Model):
 
     def __str__(self):
         return self.y1veg
+    
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "Y1veg"
+        verbose_name = "Y1veg"
+        verbose_name_plural = "Y1vegs"
     
 class Y2veg(models.Model):
     y2veg = models.FloatField(default=0.0)
@@ -34,6 +61,15 @@ class Y2veg(models.Model):
     def __str__(self):
         return self.y2veg
     
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "Y2veg"
+        verbose_name = "Y2veg"
+        verbose_name_plural = "Y2vegs"
+    
 class TeneurEnEau(models.Model):
     teneur_en_eau = models.FloatField(default=0.0)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -41,6 +77,15 @@ class TeneurEnEau(models.Model):
 
     def __str__(self):
         return self.teneur_en_eau
+    
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "TeneurEnEau"
+        verbose_name = "TeneurEnEau"
+        verbose_name_plural = "TeneurEnEaus"
 
 class HauteurFuelBed(models.Model):
     hauteur_fuel_bed = models.FloatField(default=0.0)
@@ -50,6 +95,15 @@ class HauteurFuelBed(models.Model):
     def __str__(self):
         return self.hauteur_fuel_bed
     
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "HauteurFuelBed"
+        verbose_name = "HauteurFuelBed"
+        verbose_name_plural = "HauteurFuelBeds"
+    
 class EmissiviteFuelBed(models.Model):
     emissivite_fuel_bed = models.FloatField(default=0.0)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -57,6 +111,15 @@ class EmissiviteFuelBed(models.Model):
 
     def __str__(self):
         return self.emissivite_fuel_bed
+    
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "EmissiviteFuelBed"
+        verbose_name = "EmissiviteFuelBed"
+        verbose_name_plural = "EmissiviteFuelBeds"
     
 class AbsorptionFuelBed(models.Model):
     absorption_fuel_bed = models.FloatField(default=0.0)
@@ -66,6 +129,15 @@ class AbsorptionFuelBed(models.Model):
     def __str__(self):
         return self.absorption_fuel_bed
     
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "AbsorptionFuelBed"
+        verbose_name = "AbsorptionFuelBed"
+        verbose_name_plural = "AbsorptionFuelBeds"
+    
 class FractionVolumiq(models.Model):
     fraction_volumiq = models.FloatField(default=0.0)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -74,6 +146,15 @@ class FractionVolumiq(models.Model):
     def __str__(self):
         return self.fraction_volumiq
     
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "FractionVolumiq"
+        verbose_name = "FractionVolumiq"
+        verbose_name_plural = "FractionVolumiqs"
+    
 class DensiteFuelBed(models.Model):
     densite_fuel_bed = models.FloatField(default=0.0)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -81,6 +162,15 @@ class DensiteFuelBed(models.Model):
 
     def __str__(self):
         return self.densite_fuel_bed
+    
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "DensiteFuelBed"
+        verbose_name = "DensiteFuelBed"
+        verbose_name_plural = "DensiteFuelBeds"
 
 class SurfaceSpec(models.Model):
     surface_spec = models.FloatField(default=0.0)
@@ -89,6 +179,15 @@ class SurfaceSpec(models.Model):
 
     def __str__(self):
         return self.surface_spec
+    
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "SurfaceSpec"
+        verbose_name = "SurfaceSpec"
+        verbose_name_plural = "SurfaceSpecs"
             
 class DiametreFuelBed(models.Model):
     diametre_fuel_bed = models.FloatField(default=0.0)
@@ -98,6 +197,15 @@ class DiametreFuelBed(models.Model):
     def __str__(self):
         return self.diametre_fuel_bed
     
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "DiametreFuelBed"
+        verbose_name = "DiametreFuelBed"
+        verbose_name_plural = "DiametreFuelBeds"
+    
 class ChaleurSpecFeulBed(models.Model):
     chaleur_spec_feul_bed = models.FloatField(default=0.0)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -105,6 +213,15 @@ class ChaleurSpecFeulBed(models.Model):
 
     def __str__(self):
         return self.chaleur_spec_feul_bed
+    
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "ChaleurSpecFeulBed"
+        verbose_name = "ChaleurSpecFeulBed"
+        verbose_name_plural = "ChaleurSpecFeulBeds"
     
 class ChargeSurface(models.Model):
     charge_surface = models.FloatField(default=0.0)
@@ -114,6 +231,15 @@ class ChargeSurface(models.Model):
     def __str__(self):
         return self.charge_surface
     
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "ChargeSurface"
+        verbose_name = "ChargeSurface"
+        verbose_name_plural = "ChargeSurfaces"
+    
 class HauteurFlamme(models.Model):
     hauteur_flamme = models.FloatField(default=0.0)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -121,6 +247,15 @@ class HauteurFlamme(models.Model):
 
     def __str__(self):
         return self.hauteur_flamme
+    
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "HauteurFlamme"
+        verbose_name = "HauteurFlamme"
+        verbose_name_plural = "HauteurFlammes"
 
 class TimeCombustion(models.Model):
     time_combustion = models.FloatField(default=0.0)
@@ -129,6 +264,15 @@ class TimeCombustion(models.Model):
 
     def __str__(self):
         return self.time_combustion
+
+    class Meta:
+        """
+        Meta Information
+        """        
+        app_label = "simulation"
+        db_table = "TimeCombustion"
+        verbose_name = "TimeCombustion"
+        verbose_name_plural = "TimeCombustions"
 
 
 class Simulation(models.Model):    
